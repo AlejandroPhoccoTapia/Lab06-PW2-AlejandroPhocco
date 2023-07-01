17f3bb6436a8c33bbd09e19ce7b87e77e4e27a90
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-06-30 20:23:58 -0500

    Se agrego las urls register, login y logout con su respectivo registro de usuario y logeo y comprobacion de usuario.

--- TravelloApp/TravelloPeru/urls.py
+++ TravelloApp/TravelloPeru/urls.py
@@ -7,6 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('destination/<int:id>', views.destination, name='destination'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- TravelloApp/TravelloPeru/views.py
+++ TravelloApp/TravelloPeru/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
@@ -11,3 +12,36 @@ def index(request):
 def destination(request, id):
     destination = Destination.objects.get(id=id)
     return render(request, 'destination.html', {'destination': destination})
+
+def register(request):
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+
+        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        user.save()
+
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
